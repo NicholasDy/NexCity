87898f857b07a525e1df8e82e44cb25d24899288
Nick <nicholasdy272@gmail.com>
2021-05-14 12:36:41 -0400

    combined the userform and the response, up next is combining the apidemo.js and script,js together so the page will flow.

--- public/js/script.js
+++ public/js/script.js
@@ -17,7 +17,7 @@
 //       .on( "change", function() {
 //         leaving.datepicker( "option", "maxDate", getDate( this ) );
 //       });
- 
+
 //     function getDate( element ) {
 //       var date;
 //       try {
@@ -25,37 +25,130 @@
 //       } catch( error ) {
 //         date = null;
 //       }
- 
+
 //       return date;
 //     }
 //   } );
 
+// $(function () {
+//   $("#slider-range").slider({
+//     range: true,
+//     min: 0,
+//     max: 2000,
+//     values: [75, 300],
+//     slide: function (event, ui) {
+//       $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+//     },
+//   });
+//   $("#amount").val(
+//     "$" +
+//       $("#slider-range").slider("values", 0) +
+//       " - $" +
+//       $("#slider-range").slider("values", 1)
+//   );
+// });
+
+// let startDate;
+// let returnDate;
+// let cityName;
+// let budget;
+
+// function getResponse() {
+  //   const responsePage = {
+  //     method: "GET",
+  //     url: "/response",
+  //     params: {
+  //       cityName: cityName,
+  //       budget: budget,
+  //       startDate: startDate,
+  //       returnDate: returnDate,
+  //     },
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   };
   
+  //   axios.request(responsePage)
+  //   .then( )
+  // }
 
-  $( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 2000,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
+
+// const resultSect = document.querySelector(".results-section")
+// const saveFlights = document.querySelector(".save-flight")
+// const resultFlight = document.querySelector(".results")
+
+var endCity = "Tokyo"; // later input used in case we are switching cities or building out the end point
+var endCitySymbol = "TYOA";
+var travelId = "298184";
+
+var citySymbol = "";
+var cityName = "New York"
+
+$("#api-inputs").submit(function (event) {
+  event.preventDefault();
+  startDate = document.querySelector("#leaving").value;
+  returnDate = document.querySelector("#returning").value;
+  cityName = document.querySelector("#start-city").value.trim();
+  budget = document.querySelector("#amount").value.trim();
+  $('#api-inputs').modal('hide');
+  return
+  // show buttons
+});
+
+$("#get-flights").click(gettingTheCityCode);
+// skyscanner api
+// request for the city code
+function gettingTheCityCode() {
+  const skyscannerCityName = {
+    method: "GET",
+    url: "/api/destination/cities/" + cityName,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  axios
+    .request(skyscannerCityName)
+    .then(function (response) {
+      console.log(response.data.Places[0].CityId);
+      citySymbol = response.data.Places[0].CityId;
+      gettingFlightData(citySymbol);
+    })
+
+    .catch(function (error) {
+      console.error(error);
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-  } );
+}
 
-  $('#api-inputs').submit(function(event){
-    event.preventDefault(); 
-    startDate = document.querySelector("#leaving").value
-    returnDate = document.querySelector("#returning").value
-    cityName = document.querySelector("#start-city").value.trim()
-    budget = document.querySelector("#amount").value.trim()
+function gettingFlightData() {
+  let flightoptions = {
+    method: "GET",
+    url: "api/destination/flights",
+    params:{
+      citySymbol: citySymbol,
+      endCity: endCitySymbol,
+      startDate: startDate,
+      returnDate: returnDate  
+    },
+    // we can send a post request (bad practice)
+    // query string
+    // ?symbol=SYM&endSymbol=END&startDate=whatever&returnDate=anotherDate
+    // req.query.symbol = SYM
+    // req.query.endSymbol = END
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  axios
+    .request(flightoptions)
+    .then(function (response) {
+      console.log(response.data.Carriers[0].Name);
+      // data.quotes & data.carriers
+      // generateFlightList(response)
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
 
-    let arr = []
-    arr.push({start: startDate}, {returnD: returnDate}, {cityN: cityName}, {budget:budget})
-    
-    console.log(arr)
-    
-  })
+
+// 
