9d31909e1cd36a5442fb26dd95cc58ff983d64ad
crystal-g-b <crystal.barnett1@gmail.com>
2021-05-13 20:10:54 -0400

    adding logo images. also adding html files for landing page and modal for sign up and planning form

---
+++ public/js/script.js
@@ -0,0 +1,47 @@
+$( function() {
+    var dateFormat = "mm/dd/yy",
+    leaving = $( "#leaving" )
+        .datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 3
+        })
+        .on( "change", function() {
+          returning.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      to = $( "#returning" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3
+      })
+      .on( "change", function() {
+        leaving.datepicker( "option", "maxDate", getDate( this ) );
+      });
+ 
+    function getDate( element ) {
+      var date;
+      try {
+        date = $.datepicker.parseDate( dateFormat, element.value );
+      } catch( error ) {
+        date = null;
+      }
+ 
+      return date;
+    }
+  } );
+
+  
+
+  $( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 2000,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  } );
